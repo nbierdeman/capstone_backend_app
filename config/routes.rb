8f236cd9f1a418e95594c6a6d0f2066d64a742a5
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-04 13:20:10 -0500

    Created api/sessions controller, and sessions#create route, controller action, and view

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,7 @@ Rails.application.routes.draw do
     delete "observations/:id" => "observations#destroy"
 
     post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
   end
 end
