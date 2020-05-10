435c03cf7d06add585c28d95dc7b5ca6f97426ac
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-10 11:51:42 -0500

    WIP

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -1,7 +1,13 @@
 class Api::MapsController < ApplicationController
   def directions
-    response = HTTP.get("https://api.mapbox.com/directions/v5/mapbox/cycling/#{params[:start_lng]}%2C#{params[:start_lat]}%3B#{params[:end_lng]}%2C#{params[:end_lat]}?alternatives=false&geometries=geojson&steps=true&access_token=#{Rails.application.credentials.maps_api[:mapbox_key]}")
-    @directions = response.parse
-    render "index.json.jb"
+    response1 = HTTP.get("https://api.mapbox.com/directions/v5/mapbox/cycling/#{params[:start_lng]}%2C#{params[:start_lat]}%3B#{params[:end_lng]}%2C#{params[:end_lat]}?alternatives=false&geometries=geojson&steps=true&access_token=#{Rails.application.credentials.maps_api[:mapbox_key]}")
+    @directions = response1.parse
+
+    @waypoints = @directions["routes"][0]["geometry"]["coordinates"]
+
+    response2 = HTTP.get("https://api.arrayofthings.org/api/nodes?project=chicago")
+    @nodes = response2.parse
+
+    render "directions.json.jb"
   end
 end
