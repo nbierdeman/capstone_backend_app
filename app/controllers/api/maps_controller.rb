8ff44c0f802ce23474c0c309074347a8e75e7fea
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-10 14:31:07 -0500

    @directions added; placeholder @observations added

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -3,15 +3,21 @@ class Api::MapsController < ApplicationController
     response1 = HTTP.get("https://api.mapbox.com/directions/v5/mapbox/cycling/#{params[:start_lng]}%2C#{params[:start_lat]}%3B#{params[:end_lng]}%2C#{params[:end_lat]}?alternatives=false&geometries=geojson&steps=true&access_token=#{Rails.application.credentials.maps_api[:mapbox_key]}")
     @directions = response1.parse
 
-    @waypoints = @directions["routes"][0]["geometry"]["coordinates"]
+    # @waypoints = @directions["routes"][0]["geometry"]["coordinates"]
 
-    response2 = HTTP.get("https://api.arrayofthings.org/api/nodes?project=chicago")
-    @nodes = response2.parse
+    # response2 = HTTP.get("https://api.arrayofthings.org/api/nodes?project=chicago")
+    # @nodes = response2.parse
 
-    @node_coordinates = []
-    @nodes["data"].each do |node|
-      @node_coordinates << node["location"]["geometry"]["coordinates"]
-    end
+    # @node_coordinates = []
+    # @nodes["data"].each do |node|
+    #   @node_coordinates << node["location"]["geometry"]["coordinates"]
+    # end
+
+    # response3 = HTTP.get("https://api.arrayofthings.org/api/nodes?location=within:#{@directions["routes"][0]["geometry"]["coordinates"]}")
+    # @nodes = response3.parse
+
+    response4 = HTTP.get("https://api.arrayofthings.org/api/observations?project=chicago&node=072")
+    @observations = response4.parse
 
     render "directions.json.jb"
   end
