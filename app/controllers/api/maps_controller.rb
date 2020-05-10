79bba749b91002dd1adbe42192a91467497bfadb
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-10 14:34:04 -0500

    Added @waypoints

--- app/controllers/api/maps_controller.rb
+++ app/controllers/api/maps_controller.rb
@@ -3,7 +3,7 @@ class Api::MapsController < ApplicationController
     response1 = HTTP.get("https://api.mapbox.com/directions/v5/mapbox/cycling/#{params[:start_lng]}%2C#{params[:start_lat]}%3B#{params[:end_lng]}%2C#{params[:end_lat]}?alternatives=false&geometries=geojson&steps=true&access_token=#{Rails.application.credentials.maps_api[:mapbox_key]}")
     @directions = response1.parse
 
-    # @waypoints = @directions["routes"][0]["geometry"]["coordinates"]
+    @waypoints = @directions["routes"][0]["geometry"]["coordinates"]
 
     # response2 = HTTP.get("https://api.arrayofthings.org/api/nodes?project=chicago")
     # @nodes = response2.parse
